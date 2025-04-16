325308863c3da2f16113e05b04253c61ff3e973d
Kanishkha Ramesh <146187605+Kanishkharamesh@users.noreply.github.com>
2025-04-16 21:17:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv').config()
+const express = require('express');
+const dbConnect = require('./config/dbConnect');
+const app = express()
+const PORT =process.env.PORT || 5000;
+const authRouter = require('./routes/authRoute');
+const productRouter = require('./routes/productRoute');
+const blogRouter = require('./routes/blogRoute');
+const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
+const cookieParser = require("cookie-parser");
+const morgan = require('morgan');
+
+const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+dbConnect();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use('/api/user',authRouter);
+app.use('/api/product',productRouter);
+app.use('/api/blog',blogRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () =>{
+    console.log(`Server is running at PORT ${PORT}`);
+}); 
